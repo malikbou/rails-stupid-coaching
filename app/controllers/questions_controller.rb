ce5b42771d59fc0245247b24caac0d772fb0a1d1
Malik Bouaoudia <malik@bouaoudia.com>
2022-11-14 14:46:11 +0000

    finished stupid coach exercise

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,18 @@ class QuestionsController < ApplicationController
   def hello
     @message = "Hello, how are you today?"
   end
+
+  def ask
+  end
+
+  def answer
+    @answer = params[:answer]
+    if @answer == "I am going to work"
+      @response = "Great!"
+    elsif @answer.include? "?"
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
 end
